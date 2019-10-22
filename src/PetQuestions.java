b11755c6fcf60bd813fcf386318780e8ee0365bb
amansingh88 <singhamanpreet.dc@gmail.com>
2019-10-22 15:04:53 -0400

    Pet Questions assignment

---
+++ src/PetQuestions.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PetQuestions {
+    public static void main(String[] args){
+        String name, breed;
+        int age;
+        Scanner keyboard= new Scanner(System.in);
+
+        System.out.println("Greetings. What is your pet's name?");
+        name= keyboard.next();
+
+        System.out.println("What kind of animal is "+ name + "? ");
+        breed=keyboard.next();
+
+        System.out.println("How old is "+name + "? ");
+        age=keyboard.nextInt();
+
+        System.out.println(name+" is your "+breed+ " and it is " +age);
+    }
+}
